46d3785b73e0066e076673a3716c7f1d186ba5cb
agn907 <itzme907@gmail.com>
2024-02-06 00:00:01 +0300

    Fix import order and add debounce function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function debounce(fn: Function, delay: number) {
+  let timer: number;
+
+  return (...args: unknown[]) => {
+    clearTimeout(timer);
+    timer = window.setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
 }
