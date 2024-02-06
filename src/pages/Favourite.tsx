215f4ace9617216d8f7fdbf0a941fc0d093cec0b
agn907 <itzme907@gmail.com>
2024-02-06 23:59:50 +0300

    Add no favourite notes message and tip

--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -15,10 +15,20 @@ export default function Favourite() {
         <h2 className="text-xl font-semibold">Favourites</h2>
       </div>
 
-      <ListItemContainer
-        items={favouriteNotes}
-        renderItem={(note) => <Note note={note} />}
-      />
+      <div>
+        {favouriteNotes.length === 0 && (
+          <div className="flex justify-center space-x-2 px-4 pt-32">
+            <div className="space-y-4">
+              <p className="text-center">No favourite notes yet.</p>
+              <p> Tip: Right click on the note to mark it as favourite</p>
+            </div>
+          </div>
+        )}
+        <ListItemContainer
+          items={favouriteNotes}
+          renderItem={(note) => <Note note={note} />}
+        />
+      </div>
     </div>
   );
 }
