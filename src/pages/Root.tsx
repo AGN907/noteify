a1cab8e5ab543db372a9ae162ffda702f73ee00d
agn907 <itzme907@gmail.com>
2024-02-06 21:21:05 +0300

    Refactor note update logic in Root component
    This change refactors the logic for updating a note in the Root component. It introduces a debouncedUpdateNote function to debounce the update calls, preventing unnecessary rapid updates. The update is triggered when the content of the Editor component changes.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,28 @@
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import Editor from "@/components/editor/Editor";
 import SidebarMenu from "@/components/shared/SidebarMenu";
+import { debounce } from "@/lib/utils";
+import { updateNote } from "@/redux/notes/notesSlice";
+import type { JSONContent } from "@tiptap/react";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
+  const { notes, selectedNoteId } = useAppSelector((state) => state.notes);
+  const dispatch = useAppDispatch();
+
+  const selectedNote = notes.get(selectedNoteId ?? "");
+
+  const debouncedUpdateNote = debounce(
+    ({
+      title = "",
+      content,
+    }: {
+      title: string | undefined;
+      content: JSONContent;
+    }) => dispatch(updateNote({ title, content })),
+    700,
+  );
+
   return (
     <div>
       <div>
@@ -14,7 +34,25 @@ export default function Root() {
         </div>
       </div>
       <div>
-        <Editor onChange={() => {}} content={"<p>New Note</p>"} />
+        {selectedNote ? (
+          <Editor
+            onChange={({ title = "New Note", content }) =>
+              debouncedUpdateNote({ title, content })
+            }
+            note={selectedNote}
+          />
+        ) : (
+          <div className="ml-52 flex h-full items-center justify-center pt-32">
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center space-x-2">
+                <span className="text-4xl">📝</span>
+                <p className="text-xl">
+                  Create a note or select an existing one
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
