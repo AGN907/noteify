5e7b04fff2b11b040e65f876f4cb0cfeba7dc77e
agn907 <itzme907@gmail.com>
2024-02-06 18:40:23 +0300

    Refactor Editor component to use Redux state and add editable prop

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,33 +1,50 @@
-import { Content, EditorContent, useEditor } from "@tiptap/react";
+import { useAppSelector } from "@/app/hooks";
+import { EditorContent, useEditor, type Content } from "@tiptap/react";
 import EditorToolbar, { extensions } from "./EditorToolbar";
 
 interface EditorProps {
   content: Content;
-  onChange: (content: string, title?: string) => void;
+  editable: boolean;
+  onChange: ({
+    title,
+    content,
+  }: {
+    title: string | undefined;
+    content: Content;
+  }) => void;
 }
 
 export default function Editor(props: EditorProps) {
-  const { content, onChange } = props;
+  const { content, editable, onChange } = props;
+  const { selectedNoteId } = useAppSelector((state) => state.notes);
 
-  const editor = useEditor({
-    extensions,
-    content,
-    onUpdate: ({ editor }) => {
-      const title = editor.getJSON()?.content?.[0].content?.[0].text;
+  const editor = useEditor(
+    {
+      extensions,
+      content,
+      editable,
 
-      onChange(editor.getHTML(), title);
-    },
-    editorProps: {
-      attributes: {
-        class:
-          "prose prose-sm fixed left-52 top-20 min-h-screen w-full p-2 dark:prose-invert sm:prose lg:prose-lg xl:prose-lg focus:outline-none",
+      onUpdate: ({ editor }) => {
+        const title = editor.getJSON()?.content?.[0].content?.[0].text;
+
+        onChange({
+          title,
+          content: editor.getJSON(),
+        });
+      },
+      editorProps: {
+        attributes: {
+          class:
+            "prose prose-sm fixed left-52 top-20 min-h-screen w-full p-2 dark:prose-invert sm:prose lg:prose-lg xl:prose-lg focus:outline-none",
+        },
       },
     },
-  });
+    [selectedNoteId, editable],
+  );
 
   return (
     <div className="ml-[26rem] w-full">
-      <EditorToolbar editor={editor} />
+      {editor?.isEditable && <EditorToolbar editor={editor} />}
       <div className="w-full">
         <EditorContent editor={editor} />
       </div>
