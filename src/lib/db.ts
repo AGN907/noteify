74a3b7c2e1b89b1c10d415add619e685d5515ad8
agn907 <itzme907@gmail.com>
2024-02-14 23:29:21 +0300

    refactor db methods and setAll and getAll

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,68 +13,92 @@ class Database {
     this.db = localDatabase;
   }
 
-  async get<T>(key: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .getItem(key)
-        .then((value) => {
-          return resolve(value as T);
-        })
-        .catch((error) => {
-          if (error instanceof Error) {
-            console.error(error.message);
-          }
-          reject(null);
-        });
-    });
+  async get<T>(key: string) {
+    try {
+      const result = this.db.getItem(key);
+
+      return result as T;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 
-  async set<T>(key: string, value: T): Promise<T> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .setItem(key, value)
-        .then((value) => {
-          resolve(value);
-        })
-        .catch((error) => {
-          if (error instanceof Error) {
-            console.error(error.message);
-          }
-          reject(null);
-        });
-    });
+  async getAll<T>(keys: string[]) {
+    try {
+      const result = (
+        await Promise.all(keys.map((key) => this.get<T>(key)))
+      ).reduce<T[]>((acc, value) => {
+        if (value) {
+          acc.push(value);
+          return acc;
+        }
+        return acc;
+      }, []);
+
+      return result;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 
-  async remove(key: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .removeItem(key)
-        .then(() => {
-          resolve();
-        })
-        .catch((error) => {
-          if (error instanceof Error) {
-            console.error(error.message);
-          }
-          reject();
-        });
-    });
+  async set<T>(key: string, value: T) {
+    try {
+      const result = this.db.setItem(key, value);
+
+      return result as T;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  async setAll<T extends { id: string }>(items: T[]) {
+    if (items.length === 0) return;
+    if (!items[0].id) {
+      console.error("Items must have an id!");
+      return;
+    }
+
+    try {
+      (await Promise.all(items.map((item) => this.set(item.id, item)))).reduce<
+        T[]
+      >((acc, value) => {
+        if (value) {
+          acc.push(value);
+          return acc;
+        }
+        return acc;
+      }, []);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  async remove(key: string) {
+    try {
+      this.db.removeItem(key);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 
   async clear(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .clear()
-        .then(() => {
-          resolve();
-        })
-        .catch((error) => {
-          if (error instanceof Error) {
-            console.error(error.message);
-          }
-          reject();
-        });
-    });
+    try {
+      this.db.clear();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
 
