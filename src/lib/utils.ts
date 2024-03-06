329caa2351be94f1ddc58beecf4f2ded1f13cd7f
AGN907 <itzme907@gmail.com>
2024-03-06 18:47:03 +0300

    Add dayjs calendar and updateLocale plugins

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import dayjs from "dayjs";
+import calendar from "dayjs/plugin/calendar";
 import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -19,6 +22,19 @@ export function debounce(fn: Function, delay: number) {
 }
 
 dayjs.extend(relativeTime);
+dayjs.extend(calendar);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale("en", {
+  calendar: {
+    sameDay: "[Today]",
+    nextDay: "[Tomorrow]",
+    nextWeek: "dddd",
+    lastDay: "[Yesterday]",
+    lastWeek: "dddd",
+    sameElse: "DD/MM/YYYY",
+  },
+});
 
 export function fromNow(date: number) {
   return dayjs(date).fromNow();
