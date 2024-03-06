99f355202ff5e388fac7e7038531bd9747d7a5ca
AGN907 <itzme907@gmail.com>
2024-03-06 18:44:09 +0300

    refactor: Remove tip and update item container

--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -1,6 +1,5 @@
 import { useAppSelector } from "@/app/hooks";
 import ListItemContainer from "@/components/ListItemContainer";
-import Note from "@/components/Note";
 
 export default function Favourite() {
   const { notes } = useAppSelector((state) => state.notes);
@@ -18,14 +17,10 @@ export default function Favourite() {
           <div className="flex justify-center space-x-2 px-4 pt-32">
             <div className="space-y-4">
               <p className="text-center">No favourite notes yet.</p>
-              <p> Tip: Right click on the note to mark it as favourite</p>
             </div>
           </div>
         )}
-        <ListItemContainer
-          items={favouriteNotes}
-          renderItem={(note) => <Note note={note} />}
-        />
+        <ListItemContainer type="note" items={favouriteNotes} />
       </div>
     </div>
   );
